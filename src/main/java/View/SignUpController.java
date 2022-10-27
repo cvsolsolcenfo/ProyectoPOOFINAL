45b646b17cd5a2a6f7a2713b8ee17198be045625
cvsolsolcenfo <115435577+cvsolsolcenfo@users.noreply.github.com>
2022-10-27 08:06:24 -0600

    Seguridad close

--- src/main/java/View/SignUpController.java
+++ src/main/java/View/SignUpController.java
@@ -7,6 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
 
 import java.net.URL;
 import java.util.ResourceBundle;
