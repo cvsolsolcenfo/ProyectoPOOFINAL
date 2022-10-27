16113487964a5ae70163c5ce0ee3231951e53fcb
cvsolsolcenfo <115435577+cvsolsolcenfo@users.noreply.github.com>
2022-10-27 07:28:59 -0600

    Initial commit

---
+++ src/main/java/View/DBUtils.java
@@ -0,0 +1,159 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.*;
+
+public class DBUtils {
+    public static void changeScene(ActionEvent event, String fxmlFile, String title, String username) {
+        Parent root = null;
+
+        if (username != null) {
+            try {
+                FXMLLoader loader = new FXMLLoader(DBUtils.class.getResource(fxmlFile));
+                root = loader.load();
+                LoggedInController loggedInController = loader.getController();
+                loggedInController.setUserInformation(username);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            try {
+                root = FXMLLoader.load(DBUtils.class.getResource(fxmlFile));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(new Scene(root)); //revisar esto
+        stage.show();
+    }
+
+    public static void signUpUser(ActionEvent event, String username, String password) {
+        Connection connection = null;
+        PreparedStatement psInsert = null;
+        PreparedStatement psCheckUserExists = null;
+        ResultSet resultSet = null;
+
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javafx-video", "root", "Love2997.");
+            psCheckUserExists = connection.prepareStatement("SELECT * FROM users WHERE username = ?");
+            psCheckUserExists.setString(1, username);
+            resultSet = psCheckUserExists.executeQuery();
+
+            if (resultSet.isBeforeFirst()) {
+                System.out.println("User already exists");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("You cannot use this username");
+                alert.show();
+            } else {
+                psInsert = connection.prepareStatement("INSERT INTO users (username, password, VALUES (?, ?)");
+                psInsert.setString(1, username);
+                psInsert.setString(2, password);
+                psInsert.executeUpdate();
+
+                changeScene(event, "logged-in.fxml", "Welcome", username);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            if (psCheckUserExists != null){
+                try {
+                    psCheckUserExists.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            if (psInsert != null){
+                try {
+                    psInsert.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public static void logInUser (ActionEvent event, String username, String password){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javafx-video", "root", "Love2997.");
+            preparedStatement = connection.prepareStatement("SELECT password FROM users WHERE username * ?");
+            resultSet = preparedStatement.executeQuery();
+
+            if(!resultSet.isBeforeFirst()){
+                System.out.println("User not found in the database");
+                Alert alert = new Alert (Alert.AlertType.ERROR);
+                alert.setContentText("Provided credentials are incorrect");
+                alert.show();
+
+            } else {
+                while (resultSet.next()){
+                    String retrievedPassword = resultSet.getString("password");
+                    if (retrievedPassword.equals(password)){
+                        changeScene(event,"logged-in.fxml", "Welcome", username);
+                    } else {
+                        System.out.println("Password did not match");
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setContentText("The provided credentials are incorrect");
+                        alert.show();
+
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null){
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+// cosas de connection sql se resolvieron con importar https://stackoverflow.com/questions/27459944/cant-resolve-symbol-resultset-in-intellij
+//https://stackoverflow.com/questions/24398953/error-cannot-find-symbol-for-class-connection-and-class-statement
+// se empezo a presentar error que dice download Javafx pero eso siempre estuvo y servia antes
