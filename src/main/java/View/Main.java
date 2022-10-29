23b64653346b4ee5669dd9fb89e75be57f00e1b3
cvsolsolcenfo <115435577+cvsolsolcenfo@users.noreply.github.com>
2022-10-28 21:44:45 -0600

    Cambios de archivos correctamente

--- src/main/java/View/Main.java
+++ src/main/java/View/Main.java
@@ -1,4 +1,4 @@
-package com.example.Controller;
+package View;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.example.Controller {
+module java {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
